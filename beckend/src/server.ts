10dcc434c13993259e78a67d052188b4b5f1d0e6
jonasmoura247 <jonas.moura.cavalcante@hotmail.com>
2023-01-12 12:32:52 -0300

    tratament error

--- beckend/src/server.ts
+++ beckend/src/server.ts
@@ -1,10 +1,29 @@
-import express, {request,response,NextFunction} from 'express'
-import {router} from './routes'
+import express, { Request, Response, NextFunction } from 'express'
+import 'express-async-errors';
+import { router } from './routes'
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 app.use(router);
 
-app.listen(3333, () => console.log('servidor online'));
+app.use((err: Error) => {
+
+})
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+        return res.status(400).json({
+            error:err.message
+        })
+    }
+    return res.status(500).json({
+        status: 'error',
+        message: 'internal server error.'
+    })
+})
+
+
+app.listen(3333, () => console.log('servidor online!!'));
 
